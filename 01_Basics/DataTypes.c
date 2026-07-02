fb734aa28570c7e079b68b21c0ed64c50ab2dbd4
Dnyaneshwar Shinde <dnyaneshwarsshinde1@gmail.com>
2026-07-02 19:06:41 +0530

    Add DataTypes and InputOutput examples

---
+++ 01_Basics/DataTypes.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+    char ch = 'A';
+    int i = 11;
+    float f = 90.99f;
+    double d = 78.67890;
+
+    printf("Displaying R Values of all Data Types\n\n");
+
+    printf("Character : %c\n",ch);
+    printf("Integer : %d\n",i);
+    printf("Float : %f\n",f);
+    printf("Double : %lf\n",d);
+
+    return 0;
+}
---
+++ 01_Basics/InputOutput.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+    int iNo = 0;
+
+    printf("Enter number : ");
+    scanf("%d",&iNo);
+
+    printf("You entered number : %d\n",iNo);
+
+    return 0;
+}
