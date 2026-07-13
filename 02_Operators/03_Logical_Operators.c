b132a08302c3f4d7b42b2bc8d227bd5f19bfd451
Dnyaneshwar Shinde <dnyaneshwarsshinde1@gmail.com>
2026-07-13 21:34:21 +0530

    Add logical operators

---
+++ 02_Operators/03_Logical_Operators.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main()
+{
+    int iNo1 = 0;
+    int iNo2 = 0;
+
+    printf("Enter first number : ");
+    scanf("%d",&iNo1);
+
+    printf("Enter second number : ");
+    scanf("%d",&iNo2);
+
+    printf("\nLogical Operators Result : \n");
+    printf("(1 = True, 0 = False)\n");
+
+    printf("\nLogical AND (&&) : %d",(iNo1 > iNo2) && (iNo2 > iNo1));
+    printf("\nLogical OR (||) : %d",(iNo1 > iNo2) || (iNo2 > iNo1));
+    printf("\nLogical NOT (!) : %d", !(iNo1 > iNo2));
+
+    printf("\n");
+
+    return 0;
+}
